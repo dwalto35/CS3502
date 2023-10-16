5c6863bffdb1598b95ae38f9bf996959ece6928b
Dorian Walton <80310629+dwalto35@users.noreply.github.com>
2023-10-16 17:34:20 -0400

    Create parallel.c

---
+++ parallel.c
@@ -0,0 +1,97 @@
+#include <pthread.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#define MAX 10240
+#define NUM_THREADS 10
+
+int n1, n2;
+char *s1, *s2;
+FILE *fp;
+int totalNum = 0; // This variable will hold the total count of matching substrings
+
+// Divide the string s1 into NUM_THREADS parts, and each thread processes one part
+int num_substring(int t, char *start, int len) {
+    int count = 0;
+
+    // Check for matching substrings in the assigned portion
+    for (int i = 0; i <= len - n2; i++) {
+        int j;
+        for (j = 0; j < n2; j++) {
+            if (start[i + j] != s2[j]) {
+                break; // No match, move to the next substring
+            }
+        }
+        if (j == n2) {
+            count++; // Found a matching substring
+        }
+    }
+
+    return count;
+}
+
+void *calSubStringThread(void *threadid) {
+    long tid = (long)threadid;
+    int len_per_thread = n1 / NUM_THREADS;
+    char *start = s1 + tid * len_per_thread;
+    int count = num_substring(tid, start, len_per_thread);
+    
+    // Store the count in the array for this thread
+    countArray[tid] = count;
+
+    pthread_exit(NULL);
+}
+
+int main(int argc, char *argv[]) {
+    pthread_t threads[NUM_THREADS];
+    int t, rc;
+
+    if ((fp = fopen("strings.txt", "r")) == NULL) {
+        printf("ERROR: can't open strings.txt!\n");
+        return 1;
+    }
+
+    s1 = (char *)malloc(sizeof(char) * MAX);
+    if (s1 == NULL) {
+        printf("ERROR: Out of memory!\n");
+        return -1;
+    }
+
+    s2 = (char *)malloc(sizeof(char) * MAX);
+    if (s2 == NULL) {
+        printf("ERROR: Out of memory\n");
+        return -1;
+    }
+
+    /* Read s1 and s2 from the file */
+    s1 = fgets(s1, MAX, fp);
+    s2 = fgets(s2, MAX, fp);
+    n1 = strlen(s1);  /* Length of s1 */
+    n2 = strlen(s2) - 1; /* Length of s2 */
+
+    if (s1 == NULL || s2 == NULL || n1 < n2) {
+        /* When error exit */
+        return -1;
+    }
+
+    for (t = 0; t < NUM_THREADS; t++) {
+        rc = pthread_create(&threads[t], NULL, calSubStringThread, (void *)(size_t)t);
+        if (rc) {
+            printf("ERROR; return code from pthread_create() is %d\n", rc);
+            exit(-1);
+        }
+    }
+
+    for (t = 0; t < NUM_THREADS; t++) {
+        pthread_join(threads[t], NULL);
+    }
+
+    // Sum up the counts from all threads
+    for (t = 0; t < NUM_THREADS; t++) {
+        totalNum += countArray[t];
+    }
+
+    printf("The number of substrings is: %d\n", totalNum);
+    return 1;
+}
