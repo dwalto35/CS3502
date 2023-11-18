5ca163af6dfafe4e6aba2f992a0d8afe77fa91c0
Dorian Walton <80310629+dwalto35@users.noreply.github.com>
2023-11-18 16:57:07 -0500

    Update new_module.c

--- new_module.c
+++ new_module.c
@@ -1,16 +1,15 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 
-
 int init_new_module(void)
 {
-	printk(KERN_INFO "Hello, world!\n");
-	return 0;
-
+    printk(KERN_INFO "Hello, world!\n");
+    return 0;
 }
 
-void exit_new_module(void) {
-	printk(KERN_INFO "Goodbye, world!\n");
+void exit_new_module(void)
+{
+    printk(KERN_INFO "Goodbye, world!\n");
 }
 
 module_init(init_new_module);
